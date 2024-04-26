9f4882ecb617279a139d0657d825f5dfd1a6c102
cpdlc <cpdlc@naver.com>
2024-04-27 02:17:53 +0900

    back

--- back/app.js
+++ back/app.js
@@ -35,14 +35,18 @@ if (process.env.NODE_ENV === 'production') {
   //   origin: 'http://chahome.store',
   //   credentials: true,
   // }));
+  app.use(cors({
+    origin: 'http://chahome.store',
+    credentials: true,
+  }));
 } else {
   app.use(morgan('dev'));
-  
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 }
-app.use(cors({
-  origin: ['http://localhost:3060','chahome.store'],
-  credentials: true,
-}));
+
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
