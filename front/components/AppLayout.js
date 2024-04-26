070e6920b376ce3fed9aecdbbd41483da2abc73b
cpdlc <cpdlc@naver.com>
2024-04-27 01:23:28 +0900

    front2

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -125,4 +125,17 @@ AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
+
 export default AppLayout;
