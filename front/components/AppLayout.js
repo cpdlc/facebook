7a34f24053c4a90d262f24905711f0f258aca01d
cpdlc <cpdlc@naver.com>
2024-04-27 02:04:56 +0900

    front2

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -11,7 +11,6 @@ import LoginForm from './LoginForm';
 import useInput from '../hooks/useInput';
 import HeaderCustomLogin from './HeaderCustomLogin';
 import HeaderCustomLogout from './HeaderCustomLogout';
-import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
@@ -125,18 +124,5 @@ const AppLayout = ({ children }) => {
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  const cookie = context.req ? context.req.headers.cookie : '';
-  axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
-    axios.defaults.headers.Cookie = cookie;
-  }
-  context.store.dispatch({
-    type: LOAD_MY_INFO_REQUEST,
-  });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
-});
 
 export default AppLayout;
